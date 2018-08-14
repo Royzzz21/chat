c70d0c4730d9d3c8d658a05068bad090827b410f
gerold <gerold@pato.net>
2018-08-14 17:33:15 +0800

    chatapp

---
+++ resources/assets/js/app.js
@@ -0,0 +1,59 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('chat-message', require('./components/ChatMessage.vue'));
+Vue.component('chat-log', require('./components/Chatlog.vue'));
+Vue.component('chat-composer', require('./components/ChatComposer.vue'));
+
+const app = new Vue({
+    el: '#app',
+    data : {
+        messages: [
+            {
+                message: 'Hey!',
+                user: 'John Doe'
+            },
+            {
+                message: 'Hello!',
+                user: "Jane Doe"
+            }
+        ]
+    },
+    methods: {
+        addMessage(message) {
+            // add to existing messages
+            this.messages.push(message);
+            // Persist to the database etc
+            axios.post('/messages', message).then(response => {
+                //Do whatever;
+            })
+        }
+    },
+    created () {
+        axios.get('/messages').then(response => {
+            this.messages = response.data;
+        });
+        Echo.join('chatroom')
+            .here()
+            .joining()
+            .leaving()
+            .listen('MessagePosted', (e) => {
+                console.log(e);
+            });
+    }
+});
